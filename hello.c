5c6be09c2626a1d044807cf6baede98a9c2c98eb
Eshan640 <eshanadlakha5@gmail.com>
2025-09-08 04:12:55 +0000

    hello world

---
+++ hello.c
@@ -0,0 +1,7 @@
+// write a c program to print "hello world"
+#include<stdio.h>  //preprocessor, to include standard input-output file
+int main() //execution of program 
+{
+    printf("Hello World"); 
+    return 0; 
+}
